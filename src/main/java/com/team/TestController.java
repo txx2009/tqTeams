cd100fdb1d21d7d35c0c763f9c90ef9b1e579683
Joker <917275527@qq.com>
2017-04-05 21:32:31 +0800

    test

---
+++ src/main/java/com/team/TestController.java
@@ -0,0 +1,25 @@
+package com.team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDate;
+
+/**
+ * Created by Administrator on 2017/4/5.
+ */
+@Controller
+@EnableAutoConfiguration
+public class TestController {
+    @RequestMapping("/")
+    @ResponseBody
+    String home(){
+        return "Hello World!";
+    }
+    public static void main(String[] args){
+        SpringApplication.run(TestController.class,args);
+    }
+}
