e73b42675256dc4cea45e2ff946644e9834e2ae4
Joker <917275527@qq.com>
2017-04-06 21:33:44 +0800

    test

--- src/main/java/com/team/TestController.java
+++ src/main/java/com/team/TestController.java
@@ -1,8 +1,12 @@
 package com.team;
 
+import com.team.com.team.jpa.User;
+import com.team.com.team.jpa.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -14,11 +18,20 @@ import java.time.LocalDate;
 @Controller
 @EnableAutoConfiguration
 public class TestController {
+
+    @Autowired
+    UserRepository repository;
+
     @RequestMapping("/")
     @ResponseBody
-    String home(){
+    public String home(){
         return "Hello World!!!!";
     }
+
+    public User get(Long id){
+        return (User)(StringUtils.isEmpty(id)?repository.findAll():repository.findOne(id));
+    }
+
     public static void main(String[] args){
         SpringApplication.run(TestController.class,args);
     }
---
+++ src/main/java/com/team/com/team/jpa/UserRepository.java
@@ -0,0 +1,9 @@
+package com.team.com.team.jpa;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Administrator on 2017/4/6.
+ */
+public interface UserRepository extends CrudRepository<User,Long>{
+}
